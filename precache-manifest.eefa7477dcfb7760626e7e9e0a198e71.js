9233804777e8e4e45c67846bdf62487ef6e1d156
pure-js <3690368+pure-js@users.noreply.github.com>
2020-05-23 23:27:53 +0300

    Updates

---
+++ precache-manifest.eefa7477dcfb7760626e7e9e0a198e71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417d5d9e8b3fbed12e8d37fca15631b7",
+    "url": "/spark-react-crud/index.html"
+  },
+  {
+    "revision": "5140cc9dd6e462a10d24",
+    "url": "/spark-react-crud/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "ca1f44e9d209f8fc6ae6",
+    "url": "/spark-react-crud/static/css/main.64900bf2.chunk.css"
+  },
+  {
+    "revision": "5140cc9dd6e462a10d24",
+    "url": "/spark-react-crud/static/js/2.2278078b.chunk.js"
+  },
+  {
+    "revision": "3d66a4ed243bca57a1e68168bbc08249",
+    "url": "/spark-react-crud/static/js/2.2278078b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1f44e9d209f8fc6ae6",
+    "url": "/spark-react-crud/static/js/main.d4c00782.chunk.js"
+  },
+  {
+    "revision": "a264468fafbadd3d878f",
+    "url": "/spark-react-crud/static/js/runtime-main.70b0a4fa.js"
+  }
+]);
